245eedb012704a3e90dc74e227c54375be754f4a
Yoshi <foschinilamat@gmail.com>
2025-12-20 22:04:50 +0100

    style: format routes and imports

--- client/App.tsx
+++ client/App.tsx
@@ -1,18 +1,18 @@
-
 import "./global.css";
 
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import RequireSupabaseAuth from "@/components/RequireSupabaseAuth";
+
 import { supabase } from "./lib/supabase";
 
-
-import { Toaster } from "@/components/ui/toaster";
-import { createRoot } from "react-dom/client";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import DroneDetail from "./pages/DroneDetail";
 import Catalogo from "./pages/Catalogo";
 import ServiziGIS from "./pages/ServiziGIS";
 import Login from "./pages/Login";
@@ -21,12 +21,13 @@ import DashboardAdmin from "./pages/DashboardAdmin";
 import AdminCatalog from "./pages/AdminCatalog";
 import Orders from "./pages/Orders";
 import Servizi from "./pages/Servizi";
+import Prenotazioni from "./pages/Prenotazioni";
 import Missioni from "./pages/Missioni";
 import Operatori from "./pages/Operatori";
-import Prenotazioni from "./pages/Prenotazioni";
-import AuthCallback from "./pages/AuthCallback";
-import NotFound from "./pages/NotFound";
 import AdminSettingsPage from "./pages/AdminSettingsPage";
+import AuthCallback from "./pages/AuthCallback";
+import DroneDetail from "./pages/DroneDetail";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -79,15 +80,88 @@ const App = () => {
             <Route path="/catalogo" element={<Catalogo />} />
             <Route path="/servizi" element={<ServiziGIS />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/admin" element={<DashboardAdmin />} />
-            <Route path="/admin/catalogo" element={<AdminCatalog />} />
-            <Route path="/admin/ordini" element={<Orders />} />
-            <Route path="/admin/servizi" element={<Servizi />} />
-            <Route path="/admin/prenotazioni" element={<Prenotazioni />} />
-            <Route path="/admin/missioni" element={<Missioni />} />
-            <Route path="/admin/operatori" element={<Operatori />} />
-            <Route path="/admin/impostazioni" element={<AdminSettingsPage />} />
+
+            <Route
+              path="/dashboard"
+              element={
+                <RequireSupabaseAuth>
+                  <Dashboard />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin"
+              element={
+                <RequireSupabaseAuth>
+                  <DashboardAdmin />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin/catalogo"
+              element={
+                <RequireSupabaseAuth>
+                  <AdminCatalog />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin/ordini"
+              element={
+                <RequireSupabaseAuth>
+                  <Orders />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin/servizi"
+              element={
+                <RequireSupabaseAuth>
+                  <Servizi />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin/prenotazioni"
+              element={
+                <RequireSupabaseAuth>
+                  <Prenotazioni />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin/missioni"
+              element={
+                <RequireSupabaseAuth>
+                  <Missioni />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin/operatori"
+              element={
+                <RequireSupabaseAuth>
+                  <Operatori />
+                </RequireSupabaseAuth>
+              }
+            />
+
+            <Route
+              path="/admin/impostazioni"
+              element={
+                <RequireSupabaseAuth>
+                  <AdminSettingsPage />
+                </RequireSupabaseAuth>
+              }
+            />
+
             <Route path="/auth/callback" element={<AuthCallback />} />
             <Route path="/drones/:id" element={<DroneDetail />} />
             <Route path="*" element={<NotFound />} />
@@ -97,6 +171,5 @@ const App = () => {
     </QueryClientProvider>
   );
 };
-
 
 createRoot(document.getElementById("root")!).render(<App />);
